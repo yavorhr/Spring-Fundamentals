3e7b6d6aac2f55c2f79bd24453ab2a40f70352c8
wolfie <yavorhristozov@gmail.com>
2024-09-04 16:47:15 +0300

    Create IocDemoApplication.java

---
+++ 03.  Spring Boot Introduction/ioc_demo/src/main/java/com/example/ioc_demo/IocDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.ioc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IocDemoApplication.class, args);
+  }
+}
