a8ed0209f2cdeccfbae885a276302a908beb79b6
wolfie <yavorhristozov@gmail.com>
2024-09-04 16:46:17 +0300

    Create AleBeerMachine.java

---
+++ 03.  Spring Boot Introduction/ioc_demo/src/main/java/com/example/ioc_demo/AleBeerMachine.java
@@ -0,0 +1,15 @@
+package com.example.ioc_demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AleBeerMachine implements BeerMachineIfc {
+
+  public AleBeerMachine() {
+  }
+
+  @Override
+  public String pourBeer() {
+    return "Ale machine is pouring tasteful ale beer!";
+  }
+}
