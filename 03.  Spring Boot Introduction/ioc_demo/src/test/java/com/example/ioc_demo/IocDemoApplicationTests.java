914da961895219bbcdfdad841bad79d75ba3ab87
wolfie <yavorhristozov@gmail.com>
2024-09-04 16:47:55 +0300

    init : ioc_demo

---
+++ 03.  Spring Boot Introduction/ioc_demo/src/test/java/com/example/ioc_demo/IocDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ioc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
