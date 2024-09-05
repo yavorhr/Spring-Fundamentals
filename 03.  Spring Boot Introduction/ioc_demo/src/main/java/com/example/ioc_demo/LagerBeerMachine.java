6633b08d2f4956c1a3bc0df2e6ea1d255c04e8e4
wolfie <yavorhristozov@gmail.com>
2024-09-05 08:44:33 +0300

    Update LagerBeerMachine.java
    Add  @PostConstruct and @PreDestroy examples

--- 03.  Spring Boot Introduction/ioc_demo/src/main/java/com/example/ioc_demo/LagerBeerMachine.java
+++ 03.  Spring Boot Introduction/ioc_demo/src/main/java/com/example/ioc_demo/LagerBeerMachine.java
@@ -1,5 +1,7 @@
 package com.example.ioc_demo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +13,15 @@ public class LagerBeerMachine implements BeerMachineIfc {
   @Override
   public String pourBeer() {
     return "Lager machine is pouring cold beer!";
+  }
+
+  @PostConstruct
+  public void postConstruct(){
+    System.out.println("Lager machine is ready for action!");
+  }
+
+  @PreDestroy
+  public void afterConstruct(){
+    System.out.println("Lager machine needs to be cleaned!");
   }
 }
