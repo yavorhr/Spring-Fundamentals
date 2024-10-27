fa6adc06538c44d67412e579430196cb36400459
wolfie <yavorhristozov@gmail.com>
2024-10-27 14:35:01 +0200

    Add : ArtistServiceImpl class as a service implementation
    - Created ArtistServiceImpl class to implement ArtistService interface.
    - Annotated with @Service for Spring's service layer component scanning.

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/ArtistService.java
@@ -0,0 +1,6 @@
+package com.example.musicdb.service;
+
+public interface ArtistService {
+
+  void initArtists();
+}
---
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/impl/ArtistServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.musicdb.service.impl;
+
+import com.example.musicdb.service.ArtistService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArtistServiceImpl implements ArtistService {
+
+  @Override
+  public void initArtists() {
+
+  }
+}
