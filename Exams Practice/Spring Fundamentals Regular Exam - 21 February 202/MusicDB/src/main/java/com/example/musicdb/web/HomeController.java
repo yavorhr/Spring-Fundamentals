83cc3f35ecaf069fd3504cbdc690c09213dd28f1
wolfie <yavorhristozov@gmail.com>
2024-10-28 14:33:36 +0200

    Update HomeController.java
    Add a GetMapping method to return the "index" view for the "/" endpoint.

--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/web/HomeController.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/web/HomeController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
   @GetMapping("/")
+  public String indexPage() {
+
+    return "index";
+  }
+
+  @GetMapping("/home")
   public String homePage() {
 
     return "home";
