f8423a03b724dcd2a68a3b26f42ef35c43b24fe5
wolfie <yavorhristozov@gmail.com>
2024-10-29 11:37:23 +0200

    Create ApplicationContext.java
    - Creat ApplicationContext for creating singleton beans, which will be injected as dependencies in the project
    - Add ModelMapper bean for mapping objects

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/config/ApplicationContext.java
@@ -0,0 +1,16 @@
+package com.example.musicdb.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class ApplicationContext {
+
+  @Bean
+  public ModelMapper modelMapper(){
+    return new ModelMapper();
+  }
+}
