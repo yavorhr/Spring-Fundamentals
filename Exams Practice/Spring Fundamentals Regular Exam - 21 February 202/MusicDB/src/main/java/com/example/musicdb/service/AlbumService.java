348a1692437083e21f4021fff34545d98248b549
wolfie <yavorhristozov@gmail.com>
2024-10-30 16:23:34 +0200

    Update : AlbumServiceImpl
    Add implementation of the interface method addAlbum(AddAlbumServiceModel serviceModel)

--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/AlbumService.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/AlbumService.java
@@ -1,7 +1,10 @@
 package com.example.musicdb.service;
 
+import com.example.musicdb.model.dto.service.AddAlbumServiceModel;
 import com.example.musicdb.model.entity.ArtistsNameEnum;
 
 public interface AlbumService {
   boolean validateIfAlbumExists(String name, ArtistsNameEnum artist);
+
+  void addAlbum(AddAlbumServiceModel map);
 }
--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/impl/AlbumServiceImpl.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/impl/AlbumServiceImpl.java
@@ -1,20 +1,49 @@
 package com.example.musicdb.service.impl;
 
+import com.example.musicdb.model.dto.service.AddAlbumServiceModel;
+import com.example.musicdb.model.entity.Album;
+import com.example.musicdb.model.entity.Artist;
 import com.example.musicdb.model.entity.ArtistsNameEnum;
+import com.example.musicdb.model.entity.User;
 import com.example.musicdb.repository.AlbumRepository;
 import com.example.musicdb.service.AlbumService;
+import com.example.musicdb.service.ArtistService;
+import com.example.musicdb.service.UserService;
+import com.example.musicdb.util.CurrentUser;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AlbumServiceImpl implements AlbumService {
   private final AlbumRepository albumRepository;
+  private final ModelMapper modelMapper;
+  private final UserService userService;
+  private final CurrentUser currentUser;
+  private final ArtistService artistService;
 
-  public AlbumServiceImpl(AlbumRepository albumRepository) {
+  public AlbumServiceImpl(AlbumRepository albumRepository, ModelMapper modelMapper, UserService userService, CurrentUser currentUser, ArtistService artistService) {
     this.albumRepository = albumRepository;
+    this.modelMapper = modelMapper;
+    this.userService = userService;
+    this.currentUser = currentUser;
+    this.artistService = artistService;
   }
 
   @Override
   public boolean validateIfAlbumExists(String name, ArtistsNameEnum artist) {
-    return this.albumRepository.findByNameAndArtist_Name(name,artist).isPresent();
+    return this.albumRepository.findByNameAndArtist_Name(name, artist).isPresent();
+  }
+
+  @Override
+  public void addAlbum(AddAlbumServiceModel serviceModel) {
+    Album album = this.modelMapper.map(serviceModel, Album.class);
+
+    User user = this.userService.findByUsername(currentUser.getUsername()).get();
+    album.setUser(user);
+
+    Artist artist = this.artistService.findArtist(serviceModel.getArtist());
+    album.setArtist(artist);
+
+    this.albumRepository.save(album);
   }
 }
