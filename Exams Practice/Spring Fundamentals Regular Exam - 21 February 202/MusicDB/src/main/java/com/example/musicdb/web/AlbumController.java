31bbce4d9898ed93d5f1b4072d00aff7b2b87ccb
wolfie <yavorhristozov@gmail.com>
2024-10-30 16:22:07 +0200

    Update AlbumController.java
    Add action method for @PostMapping("/albums/add")

--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/web/AlbumController.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/web/AlbumController.java
@@ -1,13 +1,72 @@
 package com.example.musicdb.web;
 
+import com.example.musicdb.model.dto.binding.AddAlbumBindingModel;
+import com.example.musicdb.model.dto.service.AddAlbumServiceModel;
+import com.example.musicdb.service.AlbumService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AlbumController {
+  private final ModelMapper mapper;
+  private final AlbumService albumService;
+
+  public AlbumController(ModelMapper mapper, AlbumService albumService) {
+    this.mapper = mapper;
+    this.albumService = albumService;
+  }
+
+  @ModelAttribute("addAlbumBindingModel")
+  public AddAlbumBindingModel addAlbumBindingModel(){
+    return new AddAlbumBindingModel();
+  }
 
   @GetMapping("/albums/add")
-  public String getAddAlbumPage(){
+  public String getAddAlbumPage(Model model) {
+    if (model.containsAttribute("albumExists")) {
+      model.addAttribute("albumExists", false);
+    }
+
     return "add-album";
+  }
+
+  @PostMapping("/albums/add")
+  public String addAlbum(@Valid AddAlbumBindingModel addAlbumBindingModel,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+
+    if (bindingResult.hasErrors()) {
+      redirectAttributes
+              .addFlashAttribute("addAlbumBindingModel", addAlbumBindingModel)
+              .addFlashAttribute("org.springframework.validation.BindingResult.addAlbumBindingModel", bindingResult);
+
+      return "redirect:add";
+    }
+
+    boolean albumExists =
+            this.albumService.validateIfAlbumExists(
+                    addAlbumBindingModel.getName(),
+                    addAlbumBindingModel.getArtist());
+
+    if (albumExists) {
+      redirectAttributes
+              .addFlashAttribute("addAlbumBindingModel", addAlbumBindingModel)
+              .addFlashAttribute("org.springframework.validation.BindingResult.addAlbumBindingModel", bindingResult)
+              .addFlashAttribute("albumExists", true);
+
+      return "redirect:add";
+    }
+
+    this.albumService.addAlbum(this.mapper.map(addAlbumBindingModel, AddAlbumServiceModel.class));
+
+    return "redirect:/home";
+
   }
 }
