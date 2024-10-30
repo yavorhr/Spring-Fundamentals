fecf1f67ed0716285f66f2675ea5e82b9d8c64be
wolfie <yavorhristozov@gmail.com>
2024-10-30 16:24:14 +0200

    Add : ArtistRepository for managing Artist entities
    - Introduced ArtistRepository extending JpaRepository to handle CRUD operations for Artist entities.
    - Annotated with @Repository for Spring Data JPA integration.

--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/repository/ArtistRepository.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/repository/ArtistRepository.java
@@ -1,9 +1,14 @@
 package com.example.musicdb.repository;
 
 import com.example.musicdb.model.entity.Artist;
+import com.example.musicdb.model.entity.ArtistsNameEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ArtistRepository  extends JpaRepository<Artist,Long> {
+
+  Optional<Artist> findByName(ArtistsNameEnum name);
 }
