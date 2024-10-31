8804adf5c097612e108497aa89f593cbc77bb477
wolfie <yavorhristozov@gmail.com>
2024-10-31 16:27:28 +0200

    Update HomeController.java
    Render all albums in /home page

--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/web/HomeController.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/web/HomeController.java
@@ -1,19 +1,33 @@
 package com.example.musicdb.web;
 
+import com.example.musicdb.model.dto.view.AlbumViewModel;
+import com.example.musicdb.service.AlbumService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class HomeController {
+  private final AlbumService albumService;
+
+  public HomeController(AlbumService albumService) {
+    this.albumService = albumService;
+  }
 
   @GetMapping("/")
   public String indexPage() {
-
     return "index";
   }
 
   @GetMapping("/home")
-  public String homePage() {
+  public String homePage(Model model) {
+
+    Collection<AlbumViewModel> allAlbums = this.albumService.findAllAlbums();
+
+    model.addAttribute("albumsViewModels",allAlbums );
+    model.addAttribute("totalCopies",this.albumService.getTotalCopies() );
 
     return "home";
   }
