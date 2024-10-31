25df525cd389495cf087e7fc785bc95df63af9b0
wolfie <yavorhristozov@gmail.com>
2024-10-31 16:28:16 +0200

    Update AlbumService.java

--- Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/AlbumService.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 21 February 202/MusicDB/src/main/java/com/example/musicdb/service/AlbumService.java
@@ -1,10 +1,19 @@
 package com.example.musicdb.service;
 
 import com.example.musicdb.model.dto.service.AddAlbumServiceModel;
+import com.example.musicdb.model.dto.view.AlbumViewModel;
 import com.example.musicdb.model.entity.ArtistsNameEnum;
+
+import java.util.Collection;
 
 public interface AlbumService {
   boolean validateIfAlbumExists(String name, ArtistsNameEnum artist);
 
   void addAlbum(AddAlbumServiceModel map);
+
+  Collection<AlbumViewModel> findAllAlbums();
+
+  void deleteAlbum(Long id);
+
+  Integer getTotalCopies();
 }
