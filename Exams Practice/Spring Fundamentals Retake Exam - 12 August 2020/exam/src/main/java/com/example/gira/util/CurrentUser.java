90ab3a61773add38ba09ba308dceaae2d10dbd8a
wolfie <yavorhristozov@gmail.com>
2024-11-02 13:34:26 +0200

    Add : CurrentUser
    - Spring @Component, which will be injected in the project where needed
    - Annotated with @SessionScope, which means that the object will be instantiated during the current Http Session

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/util/CurrentUser.java
@@ -0,0 +1,56 @@
+package com.example.gira.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class CurrentUser {
+  private Long id;
+  private String username;
+  private String password;
+  private String email;
+
+  public CurrentUser() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public CurrentUser setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public CurrentUser setPassword(String password) {
+    this.password = password;
+    return this;
+  }
+
+  public CurrentUser setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public CurrentUser setId(Long id) {
+    this.id = id;
+    return this;
+  }
+  
+  public boolean isLoggedIn() {
+    return this.id != null;
+  }
+}
