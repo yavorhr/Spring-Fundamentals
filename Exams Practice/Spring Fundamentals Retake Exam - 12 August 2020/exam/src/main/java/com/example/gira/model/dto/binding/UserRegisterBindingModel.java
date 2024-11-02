9b3c56702d4d8586198c55661f0beb9f9c110aa7
wolfie <yavorhristozov@gmail.com>
2024-11-02 15:50:51 +0200

    Create UserRegisterBindingModel.java

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/model/dto/binding/UserRegisterBindingModel.java
@@ -0,0 +1,47 @@
+package com.example.gira.model.dto.binding;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterBindingModel {
+  private String username;
+  private String password;
+  private String email;
+
+  public UserRegisterBindingModel() {
+  }
+
+  @NotNull
+  @Size(min = 3,max = 20)
+  public String getUsername() {
+    return username;
+  }
+
+  @NotNull
+  @Size(min = 3,max = 20)
+  public String getPassword() {
+    return password;
+  }
+
+  @Email
+  @NotNull
+  public String getEmail() {
+    return email;
+  }
+
+  public UserRegisterBindingModel setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public UserRegisterBindingModel setPassword(String password) {
+    this.password = password;
+    return this;
+  }
+
+  public UserRegisterBindingModel setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+}
