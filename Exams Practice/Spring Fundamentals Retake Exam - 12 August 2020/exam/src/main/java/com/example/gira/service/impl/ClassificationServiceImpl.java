aa5462b082dba99e2b4b3d898c2ce1c105bff52d
wolfie <yavorhristozov@gmail.com>
2024-11-02 15:03:58 +0200

    Add : ClassificationServiceImpl class as a service implementation
    - Created ClassificationServiceImpl class to implement ClassificationService interface.
    - Annotated with @Service for Spring's service layer component scanning.

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/service/impl/ClassificationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.gira.service.impl;
+
+import com.example.gira.service.ClassificationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassificationServiceImpl implements ClassificationService {
+}
