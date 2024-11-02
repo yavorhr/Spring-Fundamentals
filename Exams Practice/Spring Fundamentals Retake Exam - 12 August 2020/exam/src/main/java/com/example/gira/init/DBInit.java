d99624a44a945a936375585493833f019923e2c2
wolfie <yavorhristozov@gmail.com>
2024-11-02 15:50:59 +0200

    Update DBInit.java

--- Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/init/DBInit.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/init/DBInit.java
@@ -11,7 +11,7 @@ public class DBInit implements CommandLineRunner {
   public DBInit(ClassificationService classificationService) {
     this.classificationService = classificationService;
   }
-  
+
   @Override
   public void run(String... args) throws Exception {
     this.classificationService.initObjects();
