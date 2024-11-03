c17f20adfda906fc7f861b7a45962c298e5f6e2c
wolfie <yavorhristozov@gmail.com>
2024-11-03 14:19:07 +0200

    Add : Custom annotation @PasswordsMatchValidator
    Validated if password and confirm password matches on register

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/validation/PasswordsMatchValidator.java
@@ -0,0 +1,20 @@
+package com.example.gira.validation;
+
+import com.example.gira.model.dto.binding.UserRegisterBindingModel;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordsMatchValidator implements ConstraintValidator<PasswordsMatchOnRegister, UserRegisterBindingModel> {
+
+  @Override
+  public boolean isValid(UserRegisterBindingModel userRegisterBindingModel, ConstraintValidatorContext context) {
+    String password = userRegisterBindingModel.getPassword();
+    String confirmPassword = userRegisterBindingModel.getConfirmPassword();
+
+    if (password == null || confirmPassword == null) {
+      return true;
+    }
+
+    return password.equals(confirmPassword);
+  }
+}
