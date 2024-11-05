694be299db67c58b0ebf4be549b6b3f5d0447059
wolfie <yavorhristozov@gmail.com>
2024-11-05 14:48:28 +0200

    Update UserEntity.java
    remove the validation from the entity

--- Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/model/entity/UserEntity.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/model/entity/UserEntity.java
@@ -3,8 +3,7 @@ package com.example.battleships_exam.model.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "users")
@@ -18,25 +17,21 @@ public class UserEntity extends BaseEntity {
   }
 
   @Column(unique = true, nullable = false)
-  @Size(min = 3, max = 10)
   public String getUsername() {
     return username;
   }
 
   @Column(name = "full_name", nullable = false)
-  @Size(min = 5, max = 20)
   public String getFullName() {
     return fullName;
   }
 
   @Column(nullable = false)
-  @Size(min = 3)
   public String getPassword() {
     return password;
   }
 
   @Column(unique = true, nullable = false)
-  @Email
   public String getEmail() {
     return email;
   }
