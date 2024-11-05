2063332b2013ac5a39ef7fdb1c6cb48a97f492e6
wolfie <yavorhristozov@gmail.com>
2024-11-05 11:53:45 +0200

    Update HomeController.java
    Add attribute "tasks", this.taskService.findAllTasks() to the "home" view

--- Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/web/HomeController.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 12 August 2020/exam/src/main/java/com/example/gira/web/HomeController.java
@@ -1,31 +1,34 @@
 package com.example.gira.web;
 
+import com.example.gira.service.TaskService;
 import com.example.gira.util.CurrentUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
   private final CurrentUser currentUser;
+  private final TaskService taskService;
 
-  public HomeController(CurrentUser currentUser) {
+  public HomeController(CurrentUser currentUser, TaskService taskService) {
     this.currentUser = currentUser;
+    this.taskService = taskService;
   }
 
   @GetMapping("/home")
-  public String homePage() {
+  public String homePage(Model model) {
 
-//    if (!currentUser.isLoggedIn()) {
-//      return "redirect:/";
-//    }
+    if (!currentUser.isLoggedIn()) {
+      return "redirect:/";
+    }
 
+    model.addAttribute("tasks", this.taskService.findAllTasks());
     return "home";
   }
 
   @GetMapping("/")
   public String indexPage() {
-
-
     return "index";
   }
 }
