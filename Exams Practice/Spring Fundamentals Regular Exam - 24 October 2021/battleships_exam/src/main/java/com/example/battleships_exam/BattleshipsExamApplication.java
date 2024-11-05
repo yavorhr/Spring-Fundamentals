965d76ed16a8871f3e181e7dbe9829098cd32e96
wolfie <yavorhristozov@gmail.com>
2024-11-05 14:23:06 +0200

    init commit : Spring
    start new Spring project

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/BattleshipsExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.battleships_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsExamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BattleshipsExamApplication.class, args);
+  }
+
+}
