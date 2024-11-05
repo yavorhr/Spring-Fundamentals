369405e12db3f488e3aec81535c0b39685aa562d
wolfie <yavorhristozov@gmail.com>
2024-11-05 14:57:43 +0200

    Add : ShipServiceImpl class as a service implementation
    - Created ShipServiceImpl class to implement ShipService interface.
    - Annotated with @Service for Spring's service layer component scanning.

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/service/ShipService.java
@@ -0,0 +1,4 @@
+package com.example.battleships_exam.service;
+
+public interface ShipService {
+}
---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/service/impl/ShipServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.battleships_exam.service.impl;
+
+import com.example.battleships_exam.service.ShipService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShipServiceImpl implements ShipService {
+}
