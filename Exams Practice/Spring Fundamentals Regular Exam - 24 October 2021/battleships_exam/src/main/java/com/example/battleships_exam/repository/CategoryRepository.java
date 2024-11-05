207fede3f312dd4f18264a2f387f1c7f610e3dad
wolfie <yavorhristozov@gmail.com>
2024-11-05 14:53:07 +0200

    Add : CategoryRepository for managing Category entities
    - Introduced CategoryRepository extending JpaRepository to handle CRUD operations for Category entities.
    - Annotated with @Repository for Spring Data JPA integration.

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.battleships_exam.repository;
+
+import com.example.battleships_exam.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
