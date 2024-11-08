939511cc5fc619b824a6fcb1f2791bdfdd3afbd4
wolfie <yavorhristozov@gmail.com>
2024-11-08 17:42:40 +0200

    Update HomeController.java
    Add check if User is logged in - if not, it will redirect to index page

--- Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/HomeController.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/HomeController.java
@@ -1,11 +1,17 @@
 package com.example.battleships_exam.web;
 
+import com.example.battleships_exam.util.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  
+  private final CurrentUser currentUser;
+
+  public HomeController(CurrentUser currentUser) {
+    this.currentUser = currentUser;
+  }
+
   @GetMapping("/")
   public String indexPage() {
     return "index";
@@ -13,6 +19,11 @@ public class HomeController {
 
   @GetMapping("/home")
   public String homePage() {
+
+    if (!this.currentUser.isLoggedIn()) {
+      return "redirect:/";
+    }
+
     return "home";
   }
 }
