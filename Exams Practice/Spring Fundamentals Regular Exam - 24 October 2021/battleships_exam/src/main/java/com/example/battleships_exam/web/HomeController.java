1ae2671989c6197660b569b392d779ebd18f7262
wolfie <yavorhristozov@gmail.com>
2024-11-08 17:41:35 +0200

    Create HomeController.java

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.battleships_exam.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  
+  @GetMapping("/")
+  public String indexPage() {
+    return "index";
+  }
+
+  @GetMapping("/home")
+  public String homePage() {
+    return "home";
+  }
+}
