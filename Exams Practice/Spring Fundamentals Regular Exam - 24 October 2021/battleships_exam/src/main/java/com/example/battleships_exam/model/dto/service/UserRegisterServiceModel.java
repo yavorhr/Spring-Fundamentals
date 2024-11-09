8d834d6ab195d06c0191ee17f6e8d52feea4e25e
wolfie <yavorhristozov@gmail.com>
2024-11-09 15:10:19 +0200

    Add : UserRegisterServiceModel
    Service Model for transferring information from the Binding Model into the Service layer

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/model/dto/service/UserRegisterServiceModel.java
@@ -0,0 +1,48 @@
+package com.example.battleships_exam.model.dto.service;
+
+public class UserRegisterServiceModel {
+  private String username;
+  private String fullName;
+  private String email;
+  private String password;
+
+  public UserRegisterServiceModel() {
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public UserRegisterServiceModel setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public UserRegisterServiceModel setFullName(String fullName) {
+    this.fullName = fullName;
+    return this;
+  }
+
+  public UserRegisterServiceModel setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public UserRegisterServiceModel setPassword(String password) {
+    this.password = password;
+    return this;
+  }
+
+}
