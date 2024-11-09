10e490eb6c5b73aee91091b0dd03f54e3947de45
wolfie <yavorhristozov@gmail.com>
2024-11-09 17:43:55 +0200

    Update ShipController.java
    rename attribute with correct name addShipBindingModel

--- Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/ShipController.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/ShipController.java
@@ -2,6 +2,7 @@ package com.example.battleships_exam.web;
 
 import com.example.battleships_exam.model.dto.binding.AddShipBindingModel;
 import com.example.battleships_exam.model.dto.service.AddShipServiceModel;
+import com.example.battleships_exam.model.entity.enums.CategoryEnum;
 import com.example.battleships_exam.service.ShipService;
 import com.example.battleships_exam.util.CurrentUser;
 import jakarta.validation.Valid;
@@ -30,6 +31,11 @@ public class ShipController {
     return new AddShipBindingModel();
   }
 
+  @ModelAttribute("categories")
+  public CategoryEnum[] categories(){
+    return CategoryEnum.values();
+  }
+
   @GetMapping("/ships/add")
   public String addShipPage(){
 
@@ -47,7 +53,7 @@ public class ShipController {
 
     if (bindingResult.hasErrors()) {
       redirectAttributes
-              .addFlashAttribute("addTaskBindingModel", addShipBindingModel)
+              .addFlashAttribute("addShipBindingModel", addShipBindingModel)
               .addFlashAttribute("org.springframework.validation.BindingResult.addShipBindingModel", bindingResult);
 
       return "redirect:add";
