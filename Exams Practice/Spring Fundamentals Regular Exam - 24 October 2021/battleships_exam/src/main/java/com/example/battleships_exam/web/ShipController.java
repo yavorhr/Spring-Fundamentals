29d2caeeca6d0603482354a12fa2979a4a2cc093
wolfie <yavorhristozov@gmail.com>
2024-11-09 17:08:43 +0200

    Update ShipController.java
    Add @ModelAttribute("addShipBindingModel")
    Add check if Session user is logged in to be able to access "ships/add"

--- Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/ShipController.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/web/ShipController.java
@@ -1,14 +1,31 @@
 package com.example.battleships_exam.web;
 
+import com.example.battleships_exam.model.dto.binding.AddShipBindingModel;
+import com.example.battleships_exam.util.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ShipController {
+  private final CurrentUser currentUser;
+
+  public ShipController(CurrentUser currentUser) {
+    this.currentUser = currentUser;
+  }
+
+  @ModelAttribute("addShipBindingModel")
+  public AddShipBindingModel addShipBindingModel(){
+    return new AddShipBindingModel();
+  }
 
   @GetMapping("/ships/add")
   public String addShipPage(){
+
+    if (!this.currentUser.isLoggedIn()){
+      return "redirect:/users/login";
+    }
 
     return "ship-add";
   }
