5f954b1a00697643cb6c099c27920c60d08a67d4
wolfie <yavorhristozov@gmail.com>
2024-11-09 15:00:54 +0200

    Add : UniqeUsername custom anotation
    validated by UniqueUsernameValidator

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/validation/register/UniqueUsernameValidator.java
@@ -0,0 +1,22 @@
+package com.example.battleships_exam.validation.register;
+
+import com.example.battleships_exam.service.UserService;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+
+  private final UserService userService;
+
+  public UniqueUsernameValidator(UserService userService) {
+    this.userService = userService;
+  }
+
+  @Override
+  public boolean isValid(String username, ConstraintValidatorContext context) {
+    if (username == null) {
+      return true;
+    }
+    return userService.isUserNameAvailable(username);
+  }
+}
