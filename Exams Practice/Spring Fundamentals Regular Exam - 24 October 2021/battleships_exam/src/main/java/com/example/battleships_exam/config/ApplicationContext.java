190df817c9dedd0468be71356fb4a98e57c343aa
wolfie <yavorhristozov@gmail.com>
2024-11-09 15:41:07 +0200

    Create ApplicationContext.java
    Add @Bean for ModelMapper

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/config/ApplicationContext.java
@@ -0,0 +1,14 @@
+package com.example.battleships_exam.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationContext {
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+}
