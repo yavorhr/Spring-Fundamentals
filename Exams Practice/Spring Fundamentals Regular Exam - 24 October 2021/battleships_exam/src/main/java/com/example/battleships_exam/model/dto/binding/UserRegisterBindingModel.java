64530c931b2fd4575fbe99ad042475d5bc3d9f10
wolfie <yavorhristozov@gmail.com>
2024-11-09 14:54:36 +0200

    Add : UserRegisterBindingModel
    Binding Model

---
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/model/dto/binding/UserRegisterBindingModel.java
@@ -0,0 +1,69 @@
+package com.example.battleships_exam.model.dto.binding;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterBindingModel {
+  private String username;
+  private String fullName;
+  private String email;
+  private String password;
+  private String confirmPassword;
+
+  public UserRegisterBindingModel() {
+  }
+
+  @NotNull
+  @Size(min = 3,max = 10)
+  public String getUsername() {
+    return username;
+  }
+
+  @NotNull
+  @Size(min = 5,max = 20)
+  public String getFullName() {
+    return fullName;
+  }
+
+  @NotNull
+  @Email
+  public String getEmail() {
+    return email;
+  }
+
+  @NotNull
+  @Size(min = 3)
+  public String getPassword() {
+    return password;
+  }
+
+  public String getConfirmPassword() {
+    return confirmPassword;
+  }
+
+  public UserRegisterBindingModel setUsername(String username) {
+    this.username = username;
+    return this;
+  }
+
+  public UserRegisterBindingModel setFullName(String fullName) {
+    this.fullName = fullName;
+    return this;
+  }
+
+  public UserRegisterBindingModel setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public UserRegisterBindingModel setPassword(String password) {
+    this.password = password;
+    return this;
+  }
+
+  public UserRegisterBindingModel setConfirmPassword(String confirmPassword) {
+    this.confirmPassword = confirmPassword;
+    return this;
+  }
+}
