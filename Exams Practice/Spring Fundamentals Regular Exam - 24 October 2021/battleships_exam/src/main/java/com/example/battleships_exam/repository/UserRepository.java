e37eb2c984a18728f9b7b07e40b7821689d262d4
wolfie <yavorhristozov@gmail.com>
2024-11-10 15:49:10 +0200

    Update UserRepository.java

--- Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/repository/UserRepository.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.battleships_exam.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +14,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
   Optional<UserEntity> findByEmail(String email);
 
   Optional<UserEntity> findByUsernameAndPassword(String username, String password);
+
 }
