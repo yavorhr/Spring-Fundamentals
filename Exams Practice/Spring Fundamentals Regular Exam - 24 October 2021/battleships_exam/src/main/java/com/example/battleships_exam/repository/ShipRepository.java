c5b83cb6db07d93eb557051817f93de0ee3c41d6
wolfie <yavorhristozov@gmail.com>
2024-11-10 15:46:53 +0200

    Update ShipRepository.java
    add queries for finding Ships from the database

--- Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/repository/ShipRepository.java
+++ Exams Practice/Spring Fundamentals Regular Exam - 24 October 2021/battleships_exam/src/main/java/com/example/battleships_exam/repository/ShipRepository.java
@@ -2,12 +2,20 @@ package com.example.battleships_exam.repository;
 
 import com.example.battleships_exam.model.entity.Ship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ShipRepository extends JpaRepository<Ship,Long> {
 
   Optional<CategoryRepository> findByName(String name);
+
+  List<Ship> findAllByUserId(Long user_id);
+
+  @Query("SELECT s from Ship s WHERE s.user.id <> :id")
+  List<Ship> findOtherShips(@Param("id") Long id);
 }
