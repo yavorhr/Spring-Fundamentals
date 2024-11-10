1c5dadc5484b3ae11017f6bb78a6bc5c9953bc90
wolfie <yavorhristozov@gmail.com>
2024-11-10 17:03:59 +0200

    init commit
    Add Spring project

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/test/java/com/example/shopping_list_exam/ShoppingListExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping_list_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListExamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
