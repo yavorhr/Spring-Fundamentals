cc42832f5331fa9ba338677a7fca4a68f710769c
wolfie <yavorhristozov@gmail.com>
2024-11-10 17:17:29 +0200

    Create BaseEntity.java
    - Annotated with @MappedSuperclass, so the class will be not recorded in the database by Spring
    - The class is created for the purpose to be extended by the persistence classes
    - The class will only held information about the Id. It is annotated with @Id and the generated strategy is Identity

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/model/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.shopping_list_exam.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+  private Long id;
+
+  public BaseEntity() {
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long getId() {
+    return id;
+  }
+
+  public BaseEntity setId(Long id) {
+    this.id = id;
+    return this;
+  }
+}
