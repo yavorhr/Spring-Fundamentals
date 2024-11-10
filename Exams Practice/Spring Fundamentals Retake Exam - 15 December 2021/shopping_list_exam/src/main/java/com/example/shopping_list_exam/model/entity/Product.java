a5d65dd1c799afb803b081340fdcf89b844ad9bb
wolfie <yavorhristozov@gmail.com>
2024-11-10 17:27:49 +0200

    Add : Product entity

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/model/entity/Product.java
@@ -0,0 +1,71 @@
+package com.example.shopping_list_exam.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+  private String name;
+  private String description;
+  private BigDecimal price;
+  private LocalDate neededBefore;
+  private Category category;
+
+  public Product() {
+  }
+
+  @Column(unique = true,nullable = false)
+  public String getName() {
+    return name;
+  }
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  public String getDescription() {
+    return description;
+  }
+
+  @Column(nullable = false)
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  @Column(nullable = false)
+  public LocalDate getNeededBefore() {
+    return neededBefore;
+  }
+
+  @ManyToOne
+  public Category getCategory() {
+    return category;
+  }
+
+  public Product setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public Product setDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  public Product setPrice(BigDecimal price) {
+    this.price = price;
+    return this;
+  }
+
+  public Product setNeededBefore(LocalDate neededBefore) {
+    this.neededBefore = neededBefore;
+    return this;
+  }
+
+  public Product setCategory(Category category) {
+    this.category = category;
+    return this;
+  }
+}
