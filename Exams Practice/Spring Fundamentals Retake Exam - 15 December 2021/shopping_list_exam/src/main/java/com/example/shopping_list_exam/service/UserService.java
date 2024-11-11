73a0dae97359d3e1f0d554c208721b75a6647a2a
wolfie <yavorhristozov@gmail.com>
2024-11-11 12:11:53 +0200

    Add : Service logic on User register

--- Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/UserService.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/UserService.java
@@ -1,7 +1,11 @@
 package com.example.shopping_list_exam.service;
 
+import com.example.shopping_list_exam.model.service.UserRegisterServiceModel;
+
 public interface UserService {
   boolean isUserNameAvailable(String username);
 
   boolean isEmailAvailable(String email);
+
+  void registerUser(UserRegisterServiceModel map);
 }
--- Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/UserServiceImpl.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.example.shopping_list_exam.service.impl;
 
+import com.example.shopping_list_exam.model.entity.UserEntity;
+import com.example.shopping_list_exam.model.service.UserRegisterServiceModel;
 import com.example.shopping_list_exam.repository.UserRepository;
 import com.example.shopping_list_exam.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
+  private final ModelMapper modelMapper;
 
-  public UserServiceImpl(UserRepository userRepository) {
+  public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
     this.userRepository = userRepository;
+    this.modelMapper = modelMapper;
   }
 
   @Override
@@ -20,5 +25,10 @@ public class UserServiceImpl implements UserService {
   @Override
   public boolean isEmailAvailable(String email) {
     return false;
+  }
+
+  @Override
+  public void registerUser(UserRegisterServiceModel serviceModel) {
+    this.userRepository.save(this.modelMapper.map(serviceModel, UserEntity.class));
   }
 }
