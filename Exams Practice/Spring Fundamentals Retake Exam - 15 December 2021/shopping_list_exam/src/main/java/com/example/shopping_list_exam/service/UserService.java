73808ffa12d32eae511e19f23cd7c1c0bdf6b14f
wolfie <yavorhristozov@gmail.com>
2024-11-11 15:53:36 +0200

    Update UserService.java
    add findById(Long id);

--- Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/UserService.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.shopping_list_exam.service;
 
+import com.example.shopping_list_exam.model.entity.UserEntity;
 import com.example.shopping_list_exam.model.service.UserLoginServiceModel;
 import com.example.shopping_list_exam.model.service.UserRegisterServiceModel;
+
+import java.util.Optional;
 
 public interface UserService {
   boolean isUserNameAvailable(String username);
@@ -15,4 +18,6 @@ public interface UserService {
   boolean doesUserExist(UserLoginServiceModel map);
 
   void logout();
+
+  Optional<UserEntity> findById(Long id);
 }
--- Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/UserServiceImpl.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/UserServiceImpl.java
@@ -9,6 +9,8 @@ import com.example.shopping_list_exam.util.CurrentUser;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
   private final UserRepository userRepository;
@@ -58,5 +60,10 @@ public class UserServiceImpl implements UserService {
   @Override
   public void logout() {
     this.currentUser.logout();
+  }
+
+  @Override
+  public Optional<UserEntity> findById(Long id) {
+    return this.userRepository.findById(id);
   }
 }
