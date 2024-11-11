3f3d5e3a532385323acf40392ff96c1b99b15c9a
wolfie <yavorhristozov@gmail.com>
2024-11-11 11:59:11 +0200

    Add : @UniqueEmail custom validation
    will validate on register if the email property is not persisted in the database

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/validation/register/UniqueEmail.java
@@ -0,0 +1,21 @@
+package com.example.shopping_list_exam.validation.register;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueEmailValidator.class)
+public @interface UniqueEmail {
+
+  String message() default "Email is already taken";
+
+  Class<?>[] groups() default { };
+
+  Class<? extends Payload>[] payload() default { };
+}
