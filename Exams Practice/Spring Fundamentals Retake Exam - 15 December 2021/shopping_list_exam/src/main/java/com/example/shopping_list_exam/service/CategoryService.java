735fe3ca3f4b1c2872f474b0adaa6b7184312e4b
wolfie <yavorhristozov@gmail.com>
2024-11-11 11:29:23 +0200

    Add : CategoryServiceImpl class as a service implementation
    - Created CategoryServiceImpl class to implement CategoryService interface
    - Annotated with @Service for Spring's service layer component scanning

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/CategoryService.java
@@ -0,0 +1,4 @@
+package com.example.shopping_list_exam.service;
+
+public interface CategoryService {
+}
---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.shopping_list_exam.service.impl;
+
+import com.example.shopping_list_exam.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+}
