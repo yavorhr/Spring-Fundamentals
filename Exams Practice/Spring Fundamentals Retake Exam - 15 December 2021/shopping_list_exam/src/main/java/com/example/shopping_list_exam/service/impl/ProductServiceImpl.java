4ec5f7c6c0549c450034931d2fae91759f773935
wolfie <yavorhristozov@gmail.com>
2024-11-11 11:29:00 +0200

    Add : ProductServiceImpl class as a service implementation
    - Created ProductServiceImpl class to implement ProductService interface
    - Annotated with @Service for Spring's service layer component scanning

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.shopping_list_exam.service.impl;
+
+import com.example.shopping_list_exam.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}
