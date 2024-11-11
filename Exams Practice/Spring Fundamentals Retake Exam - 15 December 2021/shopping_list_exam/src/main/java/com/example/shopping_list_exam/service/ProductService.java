4a50597e63d12ba1eec7be60ba92828b705e753f
wolfie <yavorhristozov@gmail.com>
2024-11-11 15:54:26 +0200

    Add : Add Product service impl

--- Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/ProductService.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/ProductService.java
@@ -1,4 +1,9 @@
 package com.example.shopping_list_exam.service;
 
+import com.example.shopping_list_exam.model.service.AddProductServiceModel;
+
 public interface ProductService {
+  boolean doesProductExists(String name);
+
+  void addProduct(AddProductServiceModel serviceModel);
 }
--- Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/ProductServiceImpl.java
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/ProductServiceImpl.java
@@ -1,8 +1,43 @@
 package com.example.shopping_list_exam.service.impl;
 
+import com.example.shopping_list_exam.model.entity.Product;
+import com.example.shopping_list_exam.model.service.AddProductServiceModel;
+import com.example.shopping_list_exam.repository.ProductRepository;
+import com.example.shopping_list_exam.service.CategoryService;
 import com.example.shopping_list_exam.service.ProductService;
+import com.example.shopping_list_exam.service.UserService;
+import com.example.shopping_list_exam.util.CurrentUser;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+  private final ProductRepository productRepository;
+  private final ModelMapper modelMapper;
+  private final CategoryService categoryService;
+  private final UserService userService;
+  private final CurrentUser currentUser;
+
+  public ProductServiceImpl(ProductRepository productRepository, ModelMapper modelMapper, CategoryService categoryService, UserService userService, CurrentUser currentUser) {
+    this.productRepository = productRepository;
+    this.modelMapper = modelMapper;
+    this.categoryService = categoryService;
+    this.userService = userService;
+    this.currentUser = currentUser;
+  }
+
+  @Override
+  public boolean doesProductExists(String name) {
+    return this.productRepository.findByName(name).isEmpty();
+  }
+
+  @Override
+  public void addProduct(AddProductServiceModel serviceModel) {
+    var product = this.modelMapper.map(serviceModel, Product.class);
+    product.setCategory(this.categoryService.findCategoryByEnum(serviceModel.getCategory()));
+    product.setUser(this.userService.findById(this.currentUser.getId()).get());
+
+    this.productRepository.save(product);
+  }
+
 }
