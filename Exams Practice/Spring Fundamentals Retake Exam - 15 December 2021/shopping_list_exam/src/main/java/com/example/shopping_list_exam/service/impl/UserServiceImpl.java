678c0dd877ee8134103c2f2befa38fc06f1fbef3
wolfie <yavorhristozov@gmail.com>
2024-11-11 11:28:27 +0200

    Add : UserServiceImpl class as a service implementation
    - Created UserServiceImpl class to implement UserService interface
    - Annotated with @Service for Spring's service layer component scanning

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.shopping_list_exam.service.impl;
+
+import com.example.shopping_list_exam.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
