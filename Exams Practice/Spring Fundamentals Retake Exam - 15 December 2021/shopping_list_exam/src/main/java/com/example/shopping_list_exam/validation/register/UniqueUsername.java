25cff169619c5679672ff08d3e2bac39faecd61b
wolfie <yavorhristozov@gmail.com>
2024-11-11 11:56:56 +0200

    Add : @UniqueUsername custom anotation
    validated by UniqueUsernameValidator

---
+++ Exams Practice/Spring Fundamentals Retake Exam - 15 December 2021/shopping_list_exam/src/main/java/com/example/shopping_list_exam/validation/register/UniqueUsername.java
@@ -0,0 +1,22 @@
+package com.example.shopping_list_exam.validation.register;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = UniqueUsernameValidator.class)
+public @interface UniqueUsername {
+
+  String message() default "Username is already taken";
+
+  Class<?>[] groups() default { };
+
+  Class<? extends Payload>[] payload() default { };
+
+}
